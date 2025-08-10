00d4c8a14e1d9ec2d1d8f02d70021985b37f9736
Sanshui2 <1540647428@qq.com>
2025-08-11 01:14:23 +0800

    Update user.ts

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -1,18 +1,14 @@
 import type { UserConfig } from './types'
 
 export default {
-  // socials 数组用于配置社交链接
   socials: [
     {
       name: 'github',
       href: 'https://github.com/Sanshui2',
     },
   ],
-
-  // 网站信息
   site: {
-    title: 'Sanshui',
+    title: 'Sanshui\'s blog',
     author: 'Sanshui',
   },
-
 } satisfies UserConfig
