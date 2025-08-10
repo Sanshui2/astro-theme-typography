af3722f8f88b989090051897b7a889cfbd5f285c
Sanshui2 <1540647428@qq.com>
2025-08-11 00:27:38 +0800

    Update user.ts

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -7,21 +7,16 @@ import type { UserConfig } from '~/types'
 // }
 
 export default {
-  // socials 数组用于配置社交链接
   socials: [
     {
-      // name 字段是图标的名称，对于 GitHub，就用 'github'
       name: 'github',
-      // href 字段是您的链接地址
       href: 'https://github.com/Sanshui2',
     },
   ],
 
-  // 您之前想修改的网站标题也可以在这里一并完成
   site: {
-    // 将这里的标题替换为您想要的
-    title: 'Sanshui's blog', 
-    // 作者也建议修改
+    // 这里是修正后的代码
+    title: "Sanshui's blog", 
     author: 'Sanshui',
   },
 
