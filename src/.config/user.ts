912570295f3e046bbef02838574336e430647f20
Sanshui2 <1540647428@qq.com>
2025-08-11 00:55:11 +0800

    Update user.ts

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -1,6 +1,7 @@
 import type { UserConfig } from './types'
 
 export default {
+  // socials 数组用于配置社交链接
   socials: [
     {
       name: 'github',
@@ -8,8 +9,9 @@ export default {
     },
   ],
 
+  // 网站信息
   site: {
-    title: "Sanshui's blog",
+    title: 'Sanshui',
     author: 'Sanshui',
   },
 
